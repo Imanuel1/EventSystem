1c0b16f1a672016be16d71264b640c5af426b102
imanuel <toswim100@gmail.com>
2024-11-07 23:14:01 +0200

    client - feat - add ws for client + topic and parsing data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,64 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
+import useSocket from "./hook/useSocket";
+import { parseArrayToObject } from "./utils/utils";
+import {
+  addEventTopic,
+  allEventsTopic,
+  deleteEventTopic,
+  updateEventTopic,
+} from "./utils/environment";
+
+export type EventType = {
+  id: string;
+  timestamp: Date;
+  message: string;
+  source: EventSource;
+};
 
 function App() {
-  const [count, setCount] = useState(0);
+  const { on, isConnected } = useSocket();
+  const [events, setEvents] = useState<{ [key in string]: EventType }>({});
   const pageTitle = ": רשימת התראות ב24 שעות אחרונות";
+
+  useEffect(() => {
+    on(allEventsTopic, (data: string) => {
+      setEvents(parseArrayToObject(JSON.parse(data)));
+    });
+    on(updateEventTopic, (data: string) => {
+      const updatedEvent: EventType = JSON.parse(data);
+      setEvents((prev) => {
+        prev[updatedEvent.id] = updatedEvent;
+        return { ...prev };
+      });
+    });
+    on(addEventTopic, (data: string) => {
+      const newEvent: EventType = JSON.parse(data);
+      setEvents((prev) => {
+        prev[newEvent.id] = newEvent;
+        return { ...prev };
+      });
+    });
+    on(deleteEventTopic, (data: string) => {
+      const deletedEvent: EventType = JSON.parse(data);
+      setEvents((prev) => {
+        delete prev[deletedEvent.id];
+        return { ...prev };
+      });
+    });
+  }, [isConnected]);
 
   return (
     <div>
       <h3>{pageTitle}</h3>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        {Object.values(events).map((item: EventType, index: number) => (
+          <div key={index} className="card">
+            <span>{`${item.timestamp} | ${item.source} | ${item.message}`}</span>
+          </div>
+        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs"></p>
     </div>
   );
 }

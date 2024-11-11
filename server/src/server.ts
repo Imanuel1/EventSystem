9257e999e6d71edf61542a26fca7823634ce16a5
imanuel <toswim100@gmail.com>
2024-11-12 01:01:29 +0200

    client - config

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,10 +4,15 @@ import { Server } from "socket.io";
 import http from "http";
 import { port } from "./environment";
 import { socketInit } from "./middleware/socketHandler";
+import { redisSetUp } from "./redis/redis";
 
 const app = express();
 const server = http.createServer(app);
-export const io = new Server(server);
+export const io = new Server({
+  cors: {
+      origin: 'http://localhost:3000', // Replace with your client URL
+  }
+});
 
 app
   .use(cors())
@@ -15,5 +20,6 @@ app
   .use(express.urlencoded({ extended: false }))
   .listen(port, () => {
     socketInit(io);
+    redisSetUp();
     console.log(`Server is running op port ${port}`);
   });
